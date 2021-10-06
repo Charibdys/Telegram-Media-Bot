b42e39459a99364869a9f8f2634ac6974c537d6c
Charybdis <8596841-Charibdys@users.noreply.gitlab.com>
2021-10-06 00:16:44 -0400

    Added a file check to prevent reposts

--- src/media-bot.cr
+++ src/media-bot.cr
@@ -2,8 +2,16 @@ require "tourmaline"
 require "yaml"
 
 class MediaBot < Tourmaline::Client  
-  def pick_file
+  def pick_file(recent)
     path = Dir.glob("res/*").sample.to_s
+
+    if recent != 0
+      until File.info(path).modification_time < (Time.utc - recent.hours)
+        path = Dir.glob("res/*").sample.to_s
+     end
+    end
+
+    return path
   end
 
   #NOTE: If the MP4 has sound, it will be sent as a file, not as a "GIF"
@@ -15,13 +23,13 @@ class MediaBot < Tourmaline::Client
     when ".jpg", ".jpeg", ".png"
       send_photo(CHANNEL, path)
     end
+    File.touch(path)
   end
 end
 
 # Change working directory to media-bot
 # This is needed so the executable can be ran anywhere
 Dir.cd(File.dirname(__DIR__))
-
 
 CONFIG = File.open(File.expand_path("config.yaml")) do |file|
   YAML.parse(file)
@@ -30,5 +38,11 @@ end
 TOKEN = CONFIG["api-token"].to_s
 CHANNEL = CONFIG["channel-id"].to_s
 
+begin
+  recent = CONFIG["recency-limit"].to_s.to_i
+rescue
+  recent = 0
+end
+
 bot = MediaBot.new(bot_token: TOKEN)
-bot.send_file(bot.pick_file)
+bot.send_file(bot.pick_file(recent))
